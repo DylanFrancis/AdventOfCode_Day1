1ce345e8196f120b2f6f9552f9408133a8b48ea9
Dylan Francis <dylan.francis@hensoldt.net>
2021-02-14 18:22:13 +0200

    day 2 done

--- src/passwords.rs
+++ src/passwords.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::{BufReader, BufRead};
+use std::cmp::min;
 
 pub fn run() {
     let input = read_file();
@@ -12,7 +13,7 @@ pub fn run() {
         let character = parse_character(split.next().unwrap().to_string());
         let password = split.next().unwrap().to_string();
 
-        let is_valid = validate_password(min_max, character, &password);
+        let is_valid = validate_password_new_policy(min_max, character, &password);
 
         if is_valid {
             valid_password += 1;
@@ -29,8 +30,39 @@ fn parse_character(character: String) -> char {
 fn parse_min_max(min_max: String) -> (u32, u32) {
     let mut split = min_max.split("-");
     return
-        (split.next().unwrap().parse::<u32>().unwrap(),
-         split.next().unwrap().parse::<u32>().unwrap());
+        (split.next().unwrap().parse::<u32>().unwrap() - 1,
+         split.next().unwrap().parse::<u32>().unwrap() - 1);
+}
+
+fn validate_password_new_policy(min_max: (u32, u32), character: char, password: &String) -> bool {
+    let mut count = 0;
+    let mut contains = false;
+
+    for c in password.chars() {
+        if count == min_max.0 {
+            if c == character {
+                contains = true;
+            }
+            count += 1;
+            continue
+        }
+
+        if count == min_max.1 {
+            if c == character {
+                if contains {
+                    println!("false");
+                    return false;
+                }
+                else {
+                    println!("true");
+                    return true;
+                }
+            }
+            return contains;
+        }
+        count += 1;
+    }
+    return false;
 }
 
 fn validate_password(min_max: (u32, u32), character: char, password: &String) -> bool {
